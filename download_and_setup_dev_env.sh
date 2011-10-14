6ad4d35175a19ccbb2138a5442aba1da656682b3
KhasMek <Boushh@gmail.com>
2011-10-13 21:25:12 -0600

    update download_and_setup_dev_env.sh to ver 1.1

--- download_and_setup_dev_env.sh
+++ download_and_setup_dev_env.sh
@@ -3,21 +3,44 @@
 # packages for a gingerbread Android platform build enviornment. This has
 # been tested only on Ubuntu 11.04 but should work on any Debian based system.
 # Made by KhasMek 2011. Use it, don't kang it.
-# v0.1
-# TODO: add options for sudo passwd, adding custom manifests echoback
-# dialog, custom folders, setup git ssh key, custom number of threads for
-# repo sync.
+# v1.1
+# TODO: add options for sudo passwd, setup git ssh key, custom number of
+# threads for repo sync.
+#
+# What's New:
+# Ver. 1.1
+# -Fixed custom triggers- If you want to specify a custom branch or working
+#  directory you now must do that inside the script (don't think most will
+#  use this feature, so I trimmed down the fat a little bit.
+# -New way of calling manifests- CM7 is now the default, but additional ones
+#  wil now be called via ROM name and not manifest URL, this will make it
+#  easier for the average user that doesn't know how to find the manifests
+#  on GitHub, etc.
+#
+# Usage:
+#  ./download_and_setup_dev_env.sh Manifest
+#    Current available manifests are
+#	vanilla (jt1134's vanilla AOSP ROM)
+#	OMGB (r2doesinc and companies AOSP ROM)
+#	OMFGB (r2doesinc and companies AOSP based ROM)
+#	pool_party (Team Sbrissenmod's AOSP based ROM)
+#	AOSP (Googles official, stock Android ROM)
+#
+#  If no manifest is specified, CyanogenMod will be used.
+#
+# If an option is not called defaults will be used.
+# Current defaults are:
+# Working_Directory=~/Android
+# Custom_Manifest=git://github.com/CyanogenMod/android.git (CM7)
+# Manifest_Branch=gingerbread
+#
 
 #set -x
-# Setting up variables
-while getopts "d:m:b:" opt
-do
-        case "$opt" in
-                d) Working_Directory=`readlink -f "$OPTARG"`;;
-                m) Custom_Manifest=`readlink -f "$OPTARG"`;;
-		b) Manifest_Branch=`readlink -f "$OPTARG"`;;
-        esac
-done
+# Setting up variables if the user really wants to.
+# Defaults are Working_Directory="Android" and Manifest_Branch="gingerbread"
+Manifest=${1}
+Working_Directory=""
+Manifest_Branch=""
 
 if [ "$Working_Directory" = "" ]; then
         echo "[}-!-!- You have not specified a working directory, defaults will be used (Android) -!-!-{]"
@@ -48,11 +71,49 @@ if ! test -x ~/bin/repo ; then
 	chmod a+x ~/bin/repo
 fi
 
-if [ "$Custom_Manifest" = "" ]; then
-        echo "[}-!-!- You have not specified a custom platform manifest, defaults will be used (CM7) -!-!-{]"
-        Custom_Manifest=git://github.com/CyanogenMod/android.git
+if [ "$Manifest" = "" ]; then
+        echo "[}----- You have not specified a platform manifest, defaults will be used (CyanogenMod) -----{]"
+        Manifest=git://github.com/CyanogenMod/android.git
 else
-        echo "[}----- The platform manifest to be used is $Custom_Manifest ----{]"
+	if [ "$Manifest" = "vanilla" ]; then
+	        echo "[}----- You have chosen jt1134's Vanilla ROM -----{]"
+	        Manifest=git://github.com/jt1134/platform_manifest.git
+	else
+		if [ "$Manifest" = "AOSP" ]; then
+		        echo "[}----- You have chosen Googles Stock AOSP ROM -----{]"
+		        Manifest=git://android.git.kernel.org/platform/manifest.git
+		else
+			if [ "$Manifest" = "OMGB" ]; then
+			        echo "[}----- You have specified OMGB -----{]"
+			        Manifest=git://github.com/OMGB/manifest.git
+				if [ "$Manifest_Branch" = "" ]; then
+					Manifest_Branch=master
+				fi
+			else
+				if [ "$Manifest" = "OMFGB" ]; then
+				        echo "[}----- You have specified OMFGB -----{]"
+				        Manifest=git://github.com/OMFGB/manifest.git
+					if [ "$Manifest_Branch" = "" ]; then
+						Manifest_Branch=master
+					fi
+				else
+					if [ "$Manifest" = "pool_party" ]; then
+					        echo "[}----- You have specified Team Sbrissenmod's Pool Party. -----{]"
+					        Manifest=git://github.com/teamSbrissenmod/manifest.git
+						if [ "$Manifest_Branch" = "" ]; then
+							Manifest_Branch=master
+					fi
+				else
+						if [ "$Manifest" != "vanilla" ] && [ "$Manifest" != "AOSP" ] && [ "$Manifest" != "OMGB" ] && [ "$Manifest" != "OMFGB" ] && [ "$Manifest" != "pool_party" ]; then
+							echo "[}-!-!- You have not specified a valid platform manifest. -!-!-{]"
+							echo "[}-!-!- Halting -!-!-{]"
+							exit
+						fi
+					fi
+				fi
+			fi
+		fi
+	fi
 fi
 
 if [ "$Manifest_Branch" = "" ]; then
@@ -81,19 +142,19 @@ echo "[}----- Installing all programs necessary for building Android -----{]"
 sudo apt-get -q -y install git-core gnupg flex bison gperf build-essential zip curl \
      zlib1g-dev libc6-dev lib32ncurses5-dev ia32-libs x11proto-core-dev \
      libx11-dev lib32readline5-dev lib32z-dev libgl1-mesa-dev g++-multilib \
-     mingw32 tofrodos pngcrush schedtool sun-java6-jdk
+     mingw32 tofrodos pngcrush schedtool sun-java6-jdk gitk git-gui
 
 # Maybe I'll impliment this at some point, just as a reference for now
 #cd ~
 #echo "[}----- Downloading arm toolchain to ~ -----{]"
 #echo "[}----- Be sure to modify you the toolchain location in your kernel build scripts -----{]"
-#wget http://www.codesourcery.com/public/gnu_toolchain/arm-none-linux-gnueabi/arm-2009q3-67-arm-none-linux-gnueabi-i686-pc-l$
+#wget http://www.codesourcery.com/public/gnu_toolchain/arm-none-linux-gnueabi/arm-2009q3-67-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
 #tar -xjf arm-2009q3-67-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
 # rm -f arm-2009q3-67-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
 
 cd $Working_Directory
 echo "[}----- Initializing repositories -----{]"
-repo init -u $Custom_Manifest -b $Manifest_Branch
+repo init -u $Manifest -b $Manifest_Branch
 echo "[}----- Syncing repositories -----{]"
 repo sync -j20
 #echo "[}----- I like to sync once more with just one thread to make sure it synced correctly -----{]"
